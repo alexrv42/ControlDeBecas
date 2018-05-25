48758be95676cc3e78428cecaae2387c375bccf4
Alex Vilchis <alexrv99@hotmail.com>
2018-05-25 00:45:50 -0500

    Divide directorios y agrega rest api de GET

--- src/App.js
+++ src/App.js
@@ -3,8 +3,25 @@ import './App.css';
 import Example from "./Example";
 import {Appbar} from 'muicss/react'
 
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+	host: "localhost",
+	user: "empuser",
+	password: "empuser968574"
+});
 
 class App extends Component {
+
+	constructor()
+	{
+		super();
+
+		con.connect(function(err) {
+			if (err) throw err;
+			console.log("Connected!");
+		});
+	}
 
   render() {
     return (
