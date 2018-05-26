9daf1db6c9c84a56d9e27275f3917e27bc904817
Alex Vilchis <alexrv99@hotmail.com>
2018-05-26 18:14:11 -0500

    Cambia ubcación de index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
-import App from './src/client/App';
-import registerServiceWorker from './src/client/registerServiceWorker';
+import App from './client/App';
+import registerServiceWorker from './client/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
