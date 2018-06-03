69c85912f8fd18e1cd82716186ff84f9b9d03dd4
Alex Vilchis <alexrv99@hotmail.com>
2018-06-02 23:43:42 -0500

    Agrega autorización CORS

--- src/requests.js
+++ src/requests.js
@@ -23,6 +23,13 @@ var connection = config.db.get;
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
+server.use(
+	function crossOrigin(req,res,next){
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "X-Requested-With");
+		return next();
+	}
+);
 
 /*server.get('/echo/:name', function (req, res, next) {
   res.send(req.params);
